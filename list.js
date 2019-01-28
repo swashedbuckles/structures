c76e05c4f32ba92531e089efb8f7fa159557fba5
Tom Joseph <swashedbuckles@gmail.com>
2019-01-28 12:36:59 -0500

    i have no idea what i am doing. yet.

--- list.js
+++ list.js
@@ -16,4 +16,23 @@ List.prototype.isEmpty = function() {
 
 List.prototype.add = function(value) {
   var node = new Node(value);
+  if(this.head == null) {
+    return void addHead(this, node);
+  }
+  
+  linkNode(this.tail, node);
+  this.tail = node;
+}
+
+function linkNode(source, target) {
+  if(source.next !== null) {
+    return void linkNode(source.next, target);
+  }
+
+  source.next = target;
+}
+
+function addHead(list, node) {
+  list.head = node;
+  list.tail = node;
 }
