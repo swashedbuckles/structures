55180bb3c6224a8cca05cba791a1e0f284fb5654
Tom Joseph <swashedbuckles@gmail.com>
2019-02-03 16:05:12 -0500

    BST - can print a tree to the console.

---
+++ tree.js
@@ -0,0 +1,11 @@
+import BST from './lib/binary-search-tree';
+function createTree(arr) {
+  const b = new BST()
+  arr.forEach(x => b.insert(x));
+  
+  return b;
+}
+
+const values = [50, 30, 70, 20, 40, 10];
+const tree = createTree(values);
+tree.print();
