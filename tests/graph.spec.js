d9280f9aadc63dd10b790c574a55c55d64e393f7
Tom Joseph <swashedbuckles@gmail.com>
2019-02-04 22:02:48 -0500

    start defining tests for graphs

---
+++ tests/graph.spec.js
@@ -0,0 +1,23 @@
+import {describe} from 'ava-spec';
+
+describe('Graph', it => {
+  it.todo('should add nodes to a graph');
+  it.todo('should add edges between nodes on an undirected graph');
+  it.todo('should add edges between nodes on a directed graph');
+  it.todo('should find all edges connected to a node on an undirected graph')
+  it.todo('should find all edges connected to a node on a directed graph')
+  it.todo('should find neighbors of a node');
+  it.todo('should return all edges');
+  it.todo('should return all nodes');
+  it.todo('should be able to remove nodes');
+  it.todo('should be able to remove edges');
+});
+
+describe('Edges', it => {
+  it.todo('should create an edge');
+  it.todo('should be able to specify a weight on the edge');
+  it.todo('should be able to assign data to an edge');
+})
+
+describe('Nodes', it => {
+});
